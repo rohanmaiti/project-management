aa7c5a632b420de892ddf617e770ece7228c3ea8
rohanmaiti <maitirohan2019@gmail.com>
2025-11-03 18:17:06 +0530

    fix: env for vercel local + production

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? import.meta.env.VITE_DEV_APP_API_URL
+      : import.meta.env.VITE_REACT_APP_API_URL,
   withCredentials: true,
 });
